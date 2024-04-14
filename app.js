fb38096d9929a7bf59e0abf6c31a54c38c00e175
yep-pushian <yprempushian@gmail.com>
2024-04-14 19:54:35 -0400

    fixed cookie session

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ app.use(session({
     resave: false,
     saveUninitialized: false, 
     cookie: {
-        httpOnly: true, 
-        secure: process.env.NODE_ENV === 'production',
         maxAge: 24 * 60 * 60 * 1000 
     }
 
